9c46c635c29d1984f90d385c9e0e62f590430817
Lauta <lau@gmail.com>
2023-05-10 16:27:31 -0300

    Archivos en src/views/partials creados

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,34 @@
+import { readFileSync, writeFileSync } from 'fs'
+import { v4 as uuidv4 } from 'uuid';
+
 import { Router } from "express";
 export const router = Router();
 
-router.get("/", (_req, res) => res.render('index.ejs'))
+// guardar datos en un archivo .json
+
+// Cargar datos del archivo .json al iniciar la aplicacion.
+const jsonBook = readFileSync('src/book.json', 'utf-8') // Leer el book.json;
+const book = JSON.parse(jsonBook) // Investigar que hace esta linea
+
+router.get("/", (_req, res) => res.render('index.ejs', { book }))
 router.get('/new-entry', (_req, res) => res.render("new-entry"))
+
 router.post('/new-entry', (req, res) => {
-  console.log(req.body);
-  res.send("re")
+  const { Title, author, image, description } = req.body;
+  const infoBooks = { id: uuidv4(), Title, author, image, description }
+  if ( !Title || !author || !image || !description ) {
+    res.status(404).send("Libro no encontrado")
+  } else {
+    book.push(infoBooks)
+    const jsonBook = JSON.stringify(book); // Combierte el array en formato .json
+    writeFileSync("src/book.json", jsonBook, 'utf-8') // Escribe o 'guarda' el dato jsonBook en book.json
+    res.redirect("/")
+  }
+})
+
+router.get('/delete/:id', (req, res) => {
+  book.filter((b) => b !== req.params.id)
+  console.log(req.params)
+  res.send("dsa")
 })
 
